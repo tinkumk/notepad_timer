6fb53e637f8c575b1fa37f8eae7b554f20a67988
tinku <tinku.kakoti@reverieinc.com>
2018-06-16 23:22:55 +0530

    init commit

--- Notepad.py
+++ Notepad.py
@@ -3,6 +3,8 @@ import os
 from tkinter import *
 from tkinter.messagebox import *
 from tkinter.filedialog import *
+import time
+import datetime
 
 class Notepad:
 
@@ -12,12 +14,14 @@ class Notepad:
     #default window width and height
     __thisWidth = 300
     __thisHeight = 300
-    __thisTextArea = Text(__root)
+    __thisTextArea = Text(__root).pack()
     __thisMenuBar = Menu(__root)
     __thisFileMenu = Menu(__thisMenuBar,tearoff=0)
     __thisEditMenu = Menu(__thisMenuBar,tearoff=0)
     __thisHelpMenu = Menu(__thisMenuBar,tearoff=0)
     __thisScrollBar = Scrollbar(__thisTextArea)
+    # timerfield = Text(__root)
+    timer_button = Button(__root, text="Start Timer")
     __file = None
 
     def __init__(self,**kwargs):
@@ -59,30 +63,39 @@ class Notepad:
 
         #add controls (widget)
 
-        self.__thisTextArea.grid(sticky=N+E+S+W)
+        # self.__thisTextArea.grid(sticky=N + E + S + W)
 
-        self.__thisFileMenu.add_command(label="New",command=self.__newFile)
-        self.__thisFileMenu.add_command(label="Open",command=self.__openFile)
-        self.__thisFileMenu.add_command(label="Save",command=self.__saveFile)
-        self.__thisFileMenu.add_separator()
-        self.__thisFileMenu.add_command(label="Exit",command=self.__quitApplication)
-        self.__thisMenuBar.add_cascade(label="File",menu=self.__thisFileMenu)
+        # self.__thisFileMenu.add_command(label="New",command=self.__newFile)
+        # self.__thisFileMenu.add_command(label="Open",command=self.__openFile)
+        # self.__thisFileMenu.add_command(label="Save",command=self.__saveFile)
+        # self.__thisFileMenu.add_separator()
+        # self.__thisFileMenu.add_command(label="Exit",command=self.__quitApplication)
+        # self.__thisMenuBar.add_cascade(label="File",menu=self.__thisFileMenu)
 
-        self.__thisEditMenu.add_command(label="Cut",command=self.__cut)
-        self.__thisEditMenu.add_command(label="Copy",command=self.__copy)
-        self.__thisEditMenu.add_command(label="Paste",command=self.__paste)
-        self.__thisMenuBar.add_cascade(label="Edit",menu=self.__thisEditMenu)
+        # self.__thisEditMenu.add_command(label="Cut",command=self.__cut)
+        # self.__thisEditMenu.add_command(label="Copy",command=self.__copy)
+        # self.__thisEditMenu.add_command(label="Paste",command=self.__paste)
+        # self.__thisMenuBar.add_cascade(label="Edit",menu=self.__thisEditMenu)
+        #
+        # self.__thisHelpMenu.add_command(label="About Notepad",command=self.__showAbout)
+        # self.__thisMenuBar.add_cascade(label="Help",menu=self.__thisHelpMenu)
+        #
+        # self.__root.config(menu=self.__thisMenuBar)
 
-        self.__thisHelpMenu.add_command(label="About Notepad",command=self.__showAbout)
-        self.__thisMenuBar.add_cascade(label="Help",menu=self.__thisHelpMenu)
+        # self.__thisScrollBar.pack(side=RIGHT,fill=Y)
+        # self.__thisScrollBar.config(command=self.__thisTextArea.yview)
+        # self.__thisTextArea.config(yscrollcommand=self.__thisScrollBar.set)
 
-        self.__root.config(menu=self.__thisMenuBar)
+        # self.timerfield.pack(side=RIGHT,fill=Y)
 
-        self.__thisScrollBar.pack(side=RIGHT,fill=Y)
-        self.__thisScrollBar.config(command=self.__thisTextArea.yview)
-        self.__thisTextArea.config(yscrollcommand=self.__thisScrollBar.set)
-    
-        
+        # self.label = tkinter.Label(text="")
+        # self.label.pack()
+        self.timer_button.pack()
+        # self.update_button_time()
+        # self.timer_button.place(x=0,y=0)
+        # self.__root.mainloop()
+
+
     def __quitApplication(self):
         self.__root.destroy()
         #exit()
@@ -151,7 +164,11 @@ class Notepad:
         #run main application
         self.__root.mainloop()
 
-
+    def update_button_time(self):
+        millis = int(round(time.time() * 1000))
+        # print(millis)
+        self.timer_button.config(text = millis)
+        self.__root.after(1000, self.update_button_time)
 
 
 #run main application
